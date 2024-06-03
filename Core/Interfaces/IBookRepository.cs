71d593fcf238ad6ea30d4d8ae82439d0278de400
Bekassyl Karazhan <bekasyl.kar@gmail.com>
2024-06-03 21:19:08 +0400

    add custom method to repo

--- Core/Interfaces/IBookRepository.cs
+++ Core/Interfaces/IBookRepository.cs
@@ -5,12 +5,7 @@ namespace BooksStoreAPI.Core.Interfaces
 {
     public interface IBookRepository: IGenericRepository<Book>
     {
-        //Task<List<Book>> GetAllBooks();
-        //Task<Book> GetBookById(int id);
-        //Task<Book> AddBook(Book book);
-        //Task UpdateBook(Book book);
-        //Task DeleteBook(int id);
-
+        Task<List<Book>> GetBooksOrderedByTitleAsync();
     }
 
 }
